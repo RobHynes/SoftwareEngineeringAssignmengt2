1df5c025c5f66354d1651c5480828be2265b57d1
RobHynes <robert_hynes@eircom.net>
2019-11-18 21:53:17 +0000

    add Module Class

--- src/main/java/com/nuigalway/ct417/seAssignment2/Module.java
+++ src/main/java/com/nuigalway/ct417/seAssignment2/Module.java
@@ -1,5 +1,7 @@
 package com.nuigalway.ct417.seAssignment2;
 
+import org.joda.time.DateTime;
+
 public class Module {
 	
 	String name;
@@ -8,5 +10,25 @@ public class Module {
 	public Module(String modName)
 	{
 		name = modName;
+	}
+	
+	public void setName(String n)
+	{
+		name = n;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setStudents(String[] l)
+	{
+		students = l;
+	}
+	
+	public String[] getStudents()
+	{
+		return students;
 	}
 }
