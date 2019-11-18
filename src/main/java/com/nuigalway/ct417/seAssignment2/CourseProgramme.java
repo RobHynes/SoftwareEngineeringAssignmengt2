a4204fbff78bf8a2324ce091184f96b332ddfb86
RobHynes <robert_hynes@eircom.net>
2019-11-18 21:43:41 +0000

    add CourseProgramme Class

--- src/main/java/com/nuigalway/ct417/seAssignment2/CourseProgramme.java
+++ src/main/java/com/nuigalway/ct417/seAssignment2/CourseProgramme.java
@@ -10,10 +10,62 @@ public class CourseProgramme {
 	DateTime startDate;
 	DateTime endDate;
 	
-	public CourseProgramme(String name, DateTime startDate, DateTime endDate)
+	public CourseProgramme(String name, DateTime startDate, DateTime endDate, String[] moduleList, String[] studentList)
 	{
 		this.name = name;
 		this.endDate = endDate;
 		this.startDate = startDate;
+		modules = moduleList;
+		students = studentList;
+	}
+	
+	public void setName(String n)
+	{
+		name = n;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setStartDate(DateTime date)
+	{
+		startDate = date;
+	}
+	
+	public DateTime getStartDate()
+	{
+		return startDate;
+	}
+	
+	public void setEndDate(DateTime date)
+	{
+		endDate = date;
+	}
+	
+	public DateTime getEndDate()
+	{
+		return endDate;
+	}
+	
+	public void setStudents(String[] l)
+	{
+		students = l;
+	}
+	
+	public String[] getStudents()
+	{
+		return students;
+	}
+	
+	public void setModules(String[] l)
+	{
+		modules = l;
+	}
+	
+	public String[] getModules()
+	{
+		return modules;
 	}
 }
