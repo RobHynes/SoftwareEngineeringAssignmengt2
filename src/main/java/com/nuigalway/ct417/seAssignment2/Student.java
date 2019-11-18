28689b5e2ccc2d0568b9e29b91ced81d924b529d
RobHynes <robert_hynes@eircom.net>
2019-11-18 21:29:35 +0000

    add Student Class

--- src/main/java/com/nuigalway/ct417/seAssignment2/Student.java
+++ src/main/java/com/nuigalway/ct417/seAssignment2/Student.java
@@ -12,7 +12,7 @@ public class Student {
 	String course;
 	String[] modules;
 	
-	public Student(String name, int age, String DOB, int id, String username, String courseCode, String[] modArr)
+	public Student(String name, int age, DateTime DOB, int id, String username, String courseCode, String[] modArr)
 	{	
 		this.name = name;
 		this.age = age;
@@ -21,6 +21,77 @@ public class Student {
 		this.username = username;
 		course = courseCode;
 		modules = modArr;
+	}
+	
+	public void setName(String n)
+	{
+		name = n;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setAge(int num)
+	{
+		age = num;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	public void setDOB(DateTime date)
+	{
+		dateOfBirth = date;
+	}
+	
+	public DateTime getDOB()
+	{
+		return dateOfBirth;
+	}
+	
+	public void setID(int num)
+	{
+		id = num;
+	}
+	
+	public int getID()
+	{
+		return id;
+	}
+	
+//	public void setUsername(String n)
+//	{
+//		name = n;
+//	}
+	
+	public String getUsername()
+	{
+		username = name + age;
+		return username;
+	}
+	
+	public void setCourse(String courseCode)
+	{
+		course = courseCode;
+	}
+	
+	public String getCourse()
+	{
+		return course;
+	}
+	
+	public void setModules(String[] l)
+	{
+		modules = l;
+	}
+	
+	public String[] getModules()
+	{
+		return modules;
 	}
 
 }
