b01293f2bd7b7ead7d5a12edb8cc6b9875b15267
RobHynes <robert_hynes@eircom.net>
2019-11-19 13:38:25 +0000

    add Module class tests

--- src/main/java/com/nuigalway/ct417/seAssignment2/Module.java
+++ src/main/java/com/nuigalway/ct417/seAssignment2/Module.java
@@ -1,15 +1,20 @@
 package com.nuigalway.ct417.seAssignment2;
 
-import org.joda.time.DateTime;
+//import org.joda.time.DateTime;
 
 public class Module {
 	
 	String name;
-	String[] students;
+	int moduleID;
+	Student[] students;
+	CourseProgramme[] courses;
 	
-	public Module(String modName)
+	public Module(String modName, int id, Student[] students, CourseProgramme[] courses)
 	{
 		name = modName;
+		moduleID = id;
+		this.students = students;
+		this.courses = courses;
 	}
 	
 	public void setName(String n)
@@ -22,13 +27,33 @@ public class Module {
 		return name;
 	}
 	
-	public void setStudents(String[] l)
+	public void setStudents(Student[] l)
 	{
 		students = l;
 	}
 	
-	public String[] getStudents()
+	public Student[] getStudents()
 	{
 		return students;
+	}
+	
+	public void setID(int id)
+	{
+		moduleID = id;
+	}
+	
+	public int getID()
+	{
+		return moduleID;
+	}
+	
+	public void setCourses(CourseProgramme[] l)
+	{
+		courses = l;
+	}
+	
+	public CourseProgramme[] getCourses()
+	{
+		return courses;
 	}
 }
